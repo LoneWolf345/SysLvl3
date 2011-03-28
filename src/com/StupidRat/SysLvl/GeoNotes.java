4f1e37a0b7a21f6ed9f2811cbba2d421ea087e04
James Swailes <cswailes@gmail.com>
2011-03-27 21:15:05 -0500

    Added Location button to trigger GPS and display longitude and latitude in a TextView.

--- src/com/StupidRat/SysLvl/GeoNotes.java
+++ src/com/StupidRat/SysLvl/GeoNotes.java
@@ -1,19 +1,35 @@
 package com.StupidRat.SysLvl;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GeoNotes extends ListActivity {
+	
+	
+	private static final long MINIMUM_DISTANCE_CHANGE_FOR_UPDATES = 1; // in Meters
+	private static final long MINIMUM_TIME_BETWEEN_UPDATES = 1000; // in Milliseconds
+	
+	protected LocationManager locationManager;
+	protected Button retrieveLocationButton;
+	
     private static final int ACTIVITY_CREATE=0;
     private static final int ACTIVITY_EDIT=1;
     
@@ -31,8 +47,83 @@ public class GeoNotes extends ListActivity {
         mDbHelper.open();
         fillData();
         registerForContextMenu(getListView());
+        
+        // Create add note button listener
+        Button btnAddNote = (Button) findViewById(R.id.ButtonAddNote);
+		btnAddNote.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View view) {
+				createNote();
+			}
+		});
+		
+		//Location
+        retrieveLocationButton = (Button) findViewById(R.id.retrieve_location_button);
+        
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        
+        locationManager.requestLocationUpdates(
+        		LocationManager.GPS_PROVIDER, 
+        		MINIMUM_TIME_BETWEEN_UPDATES, 
+        		MINIMUM_DISTANCE_CHANGE_FOR_UPDATES,
+        		new MyLocationListener()
+        );
+        
+		retrieveLocationButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showCurrentLocation();
+			}
+		}); 
     }
     
+	protected void showCurrentLocation() {
+		
+		TextView tvLocation = (TextView) findViewById(R.id.TextViewLocationAcuracy);
+
+		Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+		if (location != null) {
+			String message = String.format(
+					"Current Location \n Longitude: %1$s \n Latitude: %2$s",
+					location.getLongitude(), location.getLatitude()
+			);
+			
+			tvLocation.setText(message);
+			//Toast.makeText(GeoNotes.this, message,
+			//		Toast.LENGTH_LONG).show();
+		}
+
+	}   
+
+	private class MyLocationListener implements LocationListener {
+
+		public void onLocationChanged(Location location) {
+			String message = String.format(
+					"New Location \n Longitude: %1$s \n Latitude: %2$s",
+					location.getLongitude(), location.getLatitude()
+			);
+			Toast.makeText(GeoNotes.this, message, Toast.LENGTH_LONG).show();
+		}
+
+		public void onStatusChanged(String s, int i, Bundle b) {
+			Toast.makeText(GeoNotes.this, "Provider status changed",
+					Toast.LENGTH_LONG).show();
+		}
+
+		public void onProviderDisabled(String s) {
+			Toast.makeText(GeoNotes.this,
+					"Provider disabled by the user. GPS turned off",
+					Toast.LENGTH_LONG).show();
+		}
+
+		public void onProviderEnabled(String s) {
+			Toast.makeText(GeoNotes.this,
+					"Provider enabled by the user. GPS turned on",
+					Toast.LENGTH_LONG).show();
+		}
+
+	}
+	
     private void fillData() {
         Cursor notesCursor = mDbHelper.fetchAllNotes();
         startManagingCursor(notesCursor);
